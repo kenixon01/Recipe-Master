3b13f55bed2bec47f726e379748d804c7438c92e
Khamilah Nixon <knixon4@ggc.edu>
2023-02-25 16:39:56 -0500

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,56 +1,52 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://cmaccephas:cmaccephas@cluster0.tkhruix.mongodb.net/?retryWrites=true&w=majority";
+const name = "Recipe-Master";
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const typeDefs = `
+  type Object {
+
+  }
+  type Mutations {
+
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+
   }
 `;
 
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
+const resolvers = {
+ Query: {
 
-  const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
+ },
+ Mutation: {
 
-  // The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+ }
+};
+
+const start = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+await client.connect;
+
+const db = client.db(name);
+
+const context = { db, }
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-  });
+    context,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
   
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+console.log(`🚀  Server ready at: ${url}`);
+
+start();
