34c877a3d8919cb43a2475007f6a8f52ffe34a58
Khamilah Nixon <knixon4@ggc.edu>
2023-04-16 20:51:03 -0400

    Fixes network error and ApolloError code 500

--- RecipeMaster/apollo.js
+++ RecipeMaster/apollo.js
@@ -1,25 +1,21 @@
-import { ApolloClient, HttpLink, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const URI = 'http://10.0.0.244:4000/';
+const URI = 'http://192.168.1.11:4000/';
 
 
 const httpLink = createHttpLink({
-  uri: URI,
+  uri: URI
 });
 
-const authLink = setContext(async (_, { headers }) => {
+const authLink = setContext(() => {
   // get the authentication token from local storage if it exists
-  const token = await AsyncStorage.getItem('token');
+  const token = AsyncStorage.getItem('token');
   // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token || '',
-    }
-  }
+  return { token }
 });
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
