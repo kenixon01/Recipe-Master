1e2e46dc04e76ec61f95cab786abf871347c8efe
Khamilah Nixon <knixon4@ggc.edu>
2023-04-25 21:29:26 -0400

    Updates Results page

--- view/components/auth/Screens/results/Results.js
+++ view/components/auth/Screens/results/Results.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { setRecentSearches } from '../../../../actions/index';
 import styles from './style'
@@ -17,26 +17,30 @@ export default function Results({navigation}){
     }
 
     const fetchRecipes = async (index) => {
-        const URL = `${ENDPOINT}q=${query}&app_id=${API_ID}&app_key=${API_KEY}&to=${index + MAX_SEARCH_RESULTS}&from=${index}`
-        const response = await fetch(URL)
-        const json = await response.json()
-        const data = json.hits
-        console.log(data)
-        if(data){
+        try {
+            const URL = `${ENDPOINT}q=${query}&app_id=${API_ID}&app_key=${API_KEY}&to=${index + MAX_SEARCH_RESULTS}&from=${index}`
+            const response = await fetch(URL)
+            const json = await response.json()
             handleRecentSearches(query)
+            return json.hits
+        } catch (error) {
+            console.error(error)
+            return []
         }
-        return json.hits
     }
 
     useEffect(() => {
-        setRecipes(fetchRecipes(currentIndex))
+        fetchRecipes(currentIndex).then(data => setRecipes(data))
     }, [])
 
     return (
         <View style = {styles.container}>
             <Text style = {styles.Title}>Results</Text>
             <View>
-                
+                {recipes.map((element, index) => {
+                    return (<Text key={index}>{element.recipe.label}</Text>)
+                })}
+                <Button title="Load More"/>
             </View>
         </View>
     );
