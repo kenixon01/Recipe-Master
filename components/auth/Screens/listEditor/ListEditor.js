f65ea70fb4ebf98fef805a4b7e74abd7342a8b98
Khamilah Nixon <knixon4@ggc.edu>
2023-04-30 12:37:36 -0400

    Changes

--- components/auth/Screens/listEditor/ListEditor.js
+++ components/auth/Screens/listEditor/ListEditor.js
@@ -1,18 +1,32 @@
-import * as React from 'react';
+import * as React from 'react'; // line not needed
 import { View, Text, TextInput, FlatList, StyleSheet } from 'react-native';
 import { IconButton } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import style from './style'
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import listReducer from './listReducer.js'
+import listReducer from './listReducer.js' // wrong directory
 
 
 export default function ListEditor({navigation}){
 
+    /*
+    Reducers, store, and actions files already exist. Edit those files to create 
+    your global states using react redux.  You do not need to create repetition and compute that information 
+    in this file.  You need to use useDispatch.  Do not define the reducer here, only use the 
+    listReducer in your dispatch.
+
+    You should also store the entire list of selected items into a global state, so that
+    it can be used on the results page.
+
+    I would recommend that you copy the contents of ListEditor.js, style.js, and listReducer.js into another location,
+    pull from main, then overrite those same 3 files in main.  Make sure that you don't make any changes
+    on the main branch.
+    */
+
     const [newItem, setNewItem] = useState('');
     const [editItem, setEditItem] = useState({ index: null, value: '' });
-    const [state, dispatch] = useReducer(listReducer, { items: [] });
+    const [state, dispatch] = useReducer(listReducer, { items: [] }); // useReducer???
     const items = useSelector((state) => state.items);
 
     function handleAddItem(e) {
@@ -64,6 +78,14 @@ export default function ListEditor({navigation}){
             style = {style.SearchButton}
             />
 
+        {/*
+            You shouldn't use HTML tags.  There are React Native component equivalents.  
+            div --> View
+            input --> TextInput
+            button --> Button
+            ul and li do not exist but a map and a Text component can do the trick
+            forms do not exist, but you do not need them
+        */}
         <div>
             <input type="text" value={newItem} onChange={(e) => setNewItem(e.target.value)} onKeyPress={handleAddItem} />
             <ul>
