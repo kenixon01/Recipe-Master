12e76c53191bafcd8bfad58e32b2ec793d62399e
aflores16 <101609102+aflores16@users.noreply.github.com>
2023-05-02 17:13:28 -0400

    bouba code

--- view/components/auth/Screens/results/Results.js
+++ view/components/auth/Screens/results/Results.js
@@ -1,91 +1,190 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Button, FlatList, SafeAreaView, Modal, ScrollView } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { setRecentSearches } from '../../../../actions/index';
-import { Tile } from '@rneui/themed';
+import * as React from "react";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  Pressable,
+  SafeAreaView,
+  Image,
+  FlatList,
+  Modal,
+} from "react-native";
+import { IconButton } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { useSelector } from "react-redux";
+import style from "./style";
+import { white } from "../../../../themes/colors";
+import { ScrollView } from "react-native-gesture-handler";
+import OpenURLButton from "./Url";
 
-import {Title} from '../../lib';
-import styles from './style'
-import { API_ID, API_KEY, ENDPOINT, MAX_SEARCH_RESULTS } from '@env'
-import style from './style';
+export default function Results({ navigation }) {
+  const apiData = useSelector((store) => store.data);
+  const apiDataLoading = useSelector((store) => store.isLoaded);
+  const [index, setIndex] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
 
-export default function Results({navigation}){
-    const [recipes, setRecipes] = useState([])
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [modalVisible, setModalVisible] = useState(false)
-    const [index, setIndex] = useState(0)
-    const query = useSelector((store) => store.query);
-
-    const dispatch = useDispatch();
-
-    const handleRecentSearches = (data) => {
-        dispatch(setRecentSearches(data))
-    }
-
-    const fetchRecipes = async (index) => {
-        try {
-            const URL = `${ENDPOINT}q=${query}&app_id=${API_ID}&app_key=${API_KEY}&to=${index + MAX_SEARCH_RESULTS}&from=${index}`
-            const response = await fetch(URL)
-            const json = await response.json()
-            handleRecentSearches(query)
-            return json.hits
-        } catch (error) {
-            console.error(error)
-            return []
-        }
-    }
-    useEffect(() => {
-        fetchRecipes(currentIndex).then(data => setRecipes(data))
-    }, [])
+  const loadingConditions = apiData.hits === undefined || apiDataLoading;
+  while (loadingConditions) {
     return (
-        <SafeAreaView style = {styles.container}>
-            <Title textAlign={"center"} fontFamily="PTSansNarrow" backgroundColor={"black"} color={"white"}> Results</Title>
-            <FlatList 
-                data={recipes}
-                renderItem={({item,index}) =>  
-                <View style = {{flex:2 , backgroundColor: 'black' }} key={index}>
-                    <Tile
-                        imageSrc={{uri: item.recipe.image}}
-                        title={item.recipe.label + " \n "+ "Calories: "+ Math.round(item.recipe.calories)}
-                        titleStyle = {{color:"white"}}
-                        caption= {item.recipe.calories}
-                        onPress={() => {
-                            setModalVisible(true)
-                            setIndex(index)}}
-                    />
-                    </View>}
-                keyExtractor={item => item.recipe.uri}
-                extraData={currentIndex}
-            /> 
-            <View style={style.modalContainer}>
-                <Modal
-                    animationType='slide'
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                        Alert.alert('Modal has been closed')
-                        setModalVisible(!modalVisible)
-                    }}>
-                    {
-                        (modalVisible) ?
-                            <View style={style.modal}>
-                                <Text style={style.recipeLabel}>{recipes[index].recipe.label}</Text>
-                                <Text style={style.recipeInfo}>Cuisine: {recipes[index].recipe.cuisineType}</Text>
-                                <Text style={style.recipeInfo}>Cook Time {recipes[index].recipe.totalTime} minutes</Text>
-                                <Text style={style.recipeInfo}>Serves {recipes[index].recipe.yield}</Text>
-                                <Text style={style.recipeInfo}>{recipes[index].recipe.calories} calories</Text>
-                                <Text style={style.label}>{recipes[index].recipe.url}</Text>
-                        
-                                {recipes[index].recipe.ingredientLines.map((ingredient, index) => {
-                                    return <Text key={index} style={style.ingredient}>{ingredient}</Text>
-                                })}
-
-                                <Button title="Hide modal" onPress={() => setModalVisible(!modalVisible)}/> 
-                            </View>
-                        : <View></View>
-                    }
-                </Modal>
-            </View>
-        </SafeAreaView>
+      <View style={styles.activityIndicator}>
+        <ActivityIndicator size={"large"} />
+      </View>
     );
+  }
+
+  return (
+    <ScrollView>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          paddingHorizontal: 20,
+          backgroundColor: white,
+        }}
+      >
+        <View style={styles.container}>
+          <Text style={style.Title}>Results</Text>
+          <Text style={style.smallText}>
+            Click each title to view the recipe
+          </Text>
+          <IconButton
+            icon={(props) => <Icon name="account-settings" {...props} />}
+            onPress={() => navigation.navigate("Settings")}
+            style={style.AccountBtn}
+          />
+          <View>
+            {apiData === undefined ? (
+              <Text>Loading...</Text>
+            ) : apiData.hits.length === 0 ? (
+              <Text>No results found</Text>
+            ) : (
+              <FlatList
+                numColumns={2}
+                columnWrapperStyle={{ justifyContent: "space-between" }}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                  marginTop: 10,
+                  paddingBottom: 50,
+                }}
+                data={apiData.hits}
+                renderItem={({ item, index }) => (
+                  <Pressable
+                    key={index}
+                    style={{ margin: 1, marginLeft: 15, flex: 1 }}
+                    onPress={() => {
+                      setIndex(index);
+                      setModalVisible(true);
+                    }}
+                  >
+                    <View style={{}}>
+                      {/* <View style={{ alignItems: "flex-end" }}>
+                              <Ionicons name="star" color="#ffd700" size={20} />
+                            </View> */}
+
+                      <Image
+                        style={{
+                          //aspectRatio: 2 / 3,
+                          height: 160,
+                          width: 160,
+                          borderRadius: 6,
+                        }}
+                        source={{ uri: item.recipe.image }}
+                      />
+                    </View>
+                    <Text
+                      style={{
+                        textAlign: "center",
+                        margin: 10,
+                        fontWeight: "bold",
+                        fontSize: "18",
+                      }}
+                    >
+                      {item.recipe.label}
+                    </Text>
+                  </Pressable>
+                )}
+                // keyExtractor={item => item.recipe.uri}
+              />
+              // );
+              // })
+            )}
+          </View>
+        </View>
+        <View style={style.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={style.centeredView}>
+              <View style={style.modalView}>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <View
+                    style={{ flex: 1, height: 1, backgroundColor: "black" }}
+                  />
+                  <View>
+                    <Text
+                      style={{
+                        width: 200,
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        color: "#B42306",
+                      }}
+                    >
+                      {apiData.hits[index].recipe.label}
+                    </Text>
+                  </View>
+                  <View
+                    style={{ flex: 1, height: 1, backgroundColor: "black" }}
+                  />
+                </View>
+                <View
+                  style={{
+                    height: 430,
+                    width: 380,
+                    borderRadius: 7,
+                    borderColor: "#B42306",
+                    borderWidth: 0.5,
+                    padding: 15,
+                    margin: 10,
+                  }}
+                >
+                  <Text style={style.modalText}>
+                    Recipe Details {"\n"}Calories {"\n"}{" "}
+                    {Math.round(apiData.hits[index].recipe.calories)}
+                  </Text>
+                  <Text style={style.modalText}>
+                    Serving Size {"\n"} {apiData.hits[index].recipe.yield}
+                  </Text>
+                  <Text style={style.modalText}>Ingredients</Text>
+
+                  {apiData.hits[index].recipe.ingredients.map((i, index) => {
+                    return <Text>{`\u2023 ${i.text}`}</Text>;
+                  })}
+
+                  <Text style={style.modalText}>
+                    {"\n"}Directions {"\n"}
+                    <OpenURLButton url={apiData.hits[index].recipe.url}>
+                      {apiData.hits[index].recipe.url}
+                    </OpenURLButton>
+                  </Text>
+                </View>
+
+                <Pressable
+                  style={[style.button, style.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={style.textStyle}>Close Window</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
 }
--- view/components/auth/Screens/results/style.js
+++ view/components/auth/Screens/results/style.js
@@ -38,7 +38,48 @@ export default styles = StyleSheet.create({
         position:'absolute',
         top: 35,
         right: 0,
-    },
+    },centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+      buttonOpen: {
+        backgroundColor: '#F194FF',
+      },
+      buttonClose: {
+        backgroundColor: '#B42306',
+      },
+      textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      modalText: {	
+        marginBottom: 5,
+        textAlign: 'center',
+        fontWeight: "400"
+      },
     
     
 });
